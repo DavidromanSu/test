30cf6a4b8d3a97d41440779fd7541dbdd97775cc
DavidromanSu <suxiaodong@suxiaodongdeMacBook-Air.local>
2016-07-03 12:56:38 +0800

    version2
    asdfsdffds

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"......");
+    NSLog(@"5454");
+    NSLog(@"sadfsadf");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
